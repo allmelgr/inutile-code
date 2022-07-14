0be56cf122c43bbfd66aff1d21f7b58b400f6ce5
ligongrong <gongrong.li@shuruitech.com>
2022-07-14 23:06:21 +0800

    fix: use mod

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod vol1;
+use vol1::gcd::gcd;
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", gcd(10, 9));
 }
--- src/vol1/gcd.rs
+++ src/vol1/gcd.rs
@@ -1,18 +1,11 @@
-/*
- *
- */
-
- fn gcd(a: i32, b: i32) -> i32 {
-     if a == 0 {
-         b
-     }
-     else if a < b {
-         gcd(b % a, a)
-     }
-     else {
-         gcd(b, a)
-     }
- }
- fn main() {
-     println!("{}", gcd(10, 9))
- }
+pub fn gcd(a: i32, b: i32) -> i32 {
+    if a % b == 0 {
+        b
+    }
+    else if a < b {
+        gcd(b % a, a)
+    }
+    else {
+        gcd(b, a)
+    }
+}
